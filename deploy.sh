75a85d98bb8fe8fce011b5579301a8c84ea2bc33
Gary Vermeulen <garyvermeulen@live.com>
2021-02-11 00:55:19 +0000

    pipeline updates

--- deploy.sh
+++ deploy.sh
@@ -29,7 +29,9 @@ RemoveExclusions() {
 
 BuildAndDeployEnv() {
     for i in "${!projectArr[@]}"; do
+        echo "${projectArr[$i]} going into env"
         if [[ "${projectArr[$i]}" == *"-env"* ]] || [[ "${projectArr[$i]}" == "common" ]]; then
+            echo "${projectArr[$i]} getting built as env"
             # check versions and see if there any updates... but for now build and publish
             echo "Building Environment Bundle: ${projectArr[$i]}"
             ./gradlew ${projectArr[$i]}:build-environment-bundle -q
@@ -37,16 +39,36 @@ BuildAndDeployEnv() {
             if [[ "$type" == "direct" ]]; then
                 ./gradlew ${projectArr[$i]}:import-bundle -PenvironmentType=$1 -PgatewayURL=$2 -PgatewayUsername=$3 -PgatewayPassword=$4 -q
             else
-                echo "deploy to portal..."
                 version=$(./gradlew ${projectArr[$i]}:getCurrentVersion -q)
+                preflight=($(PreflightChecks $2 $3 ${projectArr[$i]}))
 
-                metadata="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-environment-$version-env.metadata.yml;type=text/yml"
-                installBundle="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-environment-$version-env.install.bundle;type=application/octet-stream"
-                deleteBundle="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-environment-$version-env.delete.bundle;type=application/octet-stream"
+                IFS=':' read -a preflight <<<"${preflight}"
 
-                bundleUUID=$(curl -s -H "$authHeader" -H "accept: application/json;charset=UTF-8" -XPOST ${2%/}/$3$papi_bundle_uri -H "Content-Type: multipart/form-data" -F $metadata -F $installBundle -F $deleteBundle | jq -r .uuid)
-                echo $bundleUUID
-                curl -s -H "$authHeader" -H "Content-Type:application/json" -XPOST "${2%/}/$3$papi_deployment_uri/$bundleUUID/proxies" --data '{ "proxyUuid": "'$proxyUUID'"}'
+                if [[ "${preflight[1]}" == *"$version"* ]]; then
+                    echo "${projectArr[$i]^^} is already present on ${3^^} @version:$version"
+                    # Check where it's deployed... and deploy it if not already deployed...
+                else
+                    echo "${projectArr[$i]^^} is not present on ${3^^} @version:$version"
+                    # If preflight is empty then the bundle does not exist... create it.
+                    metadata="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-environment-$version-env.metadata.yml;type=text/yml"
+                    installBundle="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-environment-$version-env.install.bundle;type=application/octet-stream"
+                    deleteBundle="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-environment-$version-env.delete.bundle;type=application/octet-stream"
+
+                    bundleUUID=$(curl -s -H "$authHeader" -H "accept: application/json;charset=UTF-8" -XPOST ${2%/}/$3$papi_bundle_uri -H "Content-Type: multipart/form-data" -F $metadata -F $installBundle -F $deleteBundle | jq -r .uuid)
+
+                    if [[ ! -z "$preflight" ]] && [[ "${preflight[1]}" != *"$version"* ]]; then
+                        # if bundle already exists then we need to update deployment....papi_deployment_uri
+                        echo "Updating ${projectArr[$i]^^} on ${3^^} to the latest version"
+                        status=$(curl -s -XPUT -H "Content-Type: application/json;charset=UTF-8" -H "$authHeader" "${2%/}/$3$papi_deployment_uri/$bundleUUID/proxies/$proxyUUID" -d '{ "message": "string","status": "PENDING_DEPLOYMENT"}' jq -r .status)
+                        echo "${projectArr[$i]^^} $status on ${3^^}"
+                    else
+                        echo "Deploying ${projectArr[$i]^^} to ${3^^} for the first time"
+                        status=$(curl -s -H "$authHeader" -H "Content-Type:application/json" -XPOST "${2%/}/$3$papi_deployment_uri/$bundleUUID/proxies" --data '{ "proxyUuid": "'$proxyUUID'"}' | jq -r .status)
+                        echo "${projectArr[$i]^^} $status on ${3^^}"
+                    fi
+
+                fi
+
             fi
         fi
     done
@@ -59,10 +81,43 @@ BuildAndDeployServices() {
             ./gradlew ${projectArr[$i]}:build -q
             echo "Publishing: ${projectArr[$i]}"
             if [[ "$type" == "direct" ]]; then
-                ./gradlew ${projectArr[$i]}:import -PenvironmentType=$1 -PgatewayURL=$2 -PgatewayUsername=$3 -PgatewayPassword=$4 -q
+                ./gradlew ${projectArr[$i]}:import -PenvironmentType=$1 -PgatewayURL=$2 -PgatewayUsername=$3 -PgatewayPassword=$4
             else
-                echo "deploy to portal..."
+
+                #echo "deploy to portal..."
+                version=$(./gradlew ${projectArr[$i]}:getCurrentVersion -q)
+                preflight=($(PreflightChecks $2 $3 ${projectArr[$i]}))
+
+                IFS=':' read -a preflight <<<"${preflight}"
+
+                if [[ "${preflight[1]}" == *"$version"* ]]; then
+                    echo "${projectArr[$i]^^} is already present on ${3^^} @version:$version"
+                    # Check where it's deployed... and deploy it if not already deployed...
+                else
+                    echo "${projectArr[$i]^^} is not present on ${3^^} @version:$version"
+                    # If preflight is empty then the bundle does not exist... create it.
+                    echo "${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-$version.metadata.yml;type=text/yml"
+                    metadata="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-$version.metadata.yml;type=text/yml"
+                    installBundle="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-$version.install.bundle;type=application/octet-stream"
+                    deleteBundle="files=@./${projectArr[$i]}/build/gateway/bundle/${projectArr[$i]}-$version.delete.bundle;type=application/octet-stream"
+
+                    bundleUUID=$(curl -s -H "$authHeader" -H "accept: application/json;charset=UTF-8" -XPOST ${2%/}/$3$papi_bundle_uri -H "Content-Type: multipart/form-data" -F $metadata -F $installBundle -F $deleteBundle) # | jq -r .uuid)
+                    echo $bundleUUID
+                    if [[ ! -z "$preflight" ]] && [[ "${preflight[1]}" != *"$version"* ]]; then
+                        # if bundle already exists then we need to update deployment....papi_deployment_uri
+                        echo "Updating ${projectArr[$i]^^} on ${3^^} to the latest version"
+                        status=$(curl -s -XPUT -H "Content-Type: application/json;charset=UTF-8" -H "$authHeader" "${2%/}/$3$papi_deployment_uri/$bundleUUID/proxies/$proxyUUID" -d '{ "message": "string","status": "PENDING_DEPLOYMENT"}' | jq -r .status)
+                        echo "${projectArr[$i]^^} $status on ${3^^}"
+                    else
+                        echo "Deploying ${projectArr[$i]^^} to ${3^^} for the first time"
+                        status=$(curl -s -H "$authHeader" -H "Content-Type:application/json" -XPOST "${2%/}/$3$papi_deployment_uri/$bundleUUID/proxies" --data '{ "proxyUuid": "'$proxyUUID'"}' | jq -r .status)
+                        echo "${projectArr[$i]^^} $status on ${3^^}"
+                    fi
+
+                fi
+
             fi
+
         fi
     done
 }
@@ -121,6 +176,36 @@ RetrieveProxyUUID() {
     #echo $proxyUUID # | jq -r --arg proxyName $3 '.[] | select(.name==$proxyName).uuid'
 }
 
+PreflightChecks() {
+    bundle=$(curl -sk --header "$authHeader" --request GET ${1%/}/$2$papi_bundle_uri?name=$3)
+    bundleUUID=$(echo $bundle | jq -r --arg moduleName $3 '.results[] | select(.moduleName==$moduleName).uuid')
+
+    if [[ ! -z "$bundleUUID" ]]; then
+        version=$(echo $bundle | jq -r --arg moduleName $3 '.results[] | select(.moduleName==$moduleName).version')
+    else
+        bundle=""
+    fi
+
+    if [[ ! -z $bundle ]]; then
+        echo "$bundleUUID:$version"
+    fi
+}
+
+PreflightChecks1() {
+    bundle=$(curl -sk --header "$authHeader" --request GET ${1%/}/$2$papi_bundle_uri?name=$3)
+    bundleUUID=$(echo $bundle | jq -r --arg moduleName $3 '.results[] | select(.moduleName==$moduleName).uuid')
+
+    if [[ ! -z "$bundleUUID" ]]; then
+        version=$(echo $bundle | jq -r --arg moduleName $3 '.results[] | select(.moduleName==$moduleName).version')
+    else
+        bundle=""
+    fi
+
+    if [[ ! -z $bundle ]]; then
+        echo "$bundleUUID:$version"
+    fi
+}
+
 RemoveExclusions
 
 SetEnvironmentDetails() {
@@ -137,11 +222,11 @@ SetEnvironmentDetails() {
         tenantId=${tenantUrl#https://}
         tenantId=${tenantId%%.*}
         RetrieveProxyUUID $papiUrl $tenantId $environment
-        BuildAndDeployEnv $type $papiUrl $tenantId $environment
-        #BuildAndDeployServices $type
+        PreflightChecks1 $papiUrl $tenantId "banking"
+        #BuildAndDeployEnv $type $papiUrl $tenantId $environment
+        BuildAndDeployServices $type $papiUrl $tenantId $environment
         #DeployToProxy
     else
-        echo "Direct.."
         environment=$1
         gatewayUrl=$2
         gatewayUsername=$3
@@ -155,4 +240,4 @@ SetEnvironmentDetails() {
     fi
 }
 
-SetEnvironmentDetails $2 $3 $4 $5 $6 $7 $8
+SetEnvironmentDetails $2 $3 $4 $5 $6 $7 $8 $9
